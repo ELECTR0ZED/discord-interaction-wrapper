4513abfdd55b0b95af4815ffd41dba41c615a207
ELECTR0ZED <ELECTR0ZED@users.noreply.github.com>
2024-03-18 22:52:04 +0000

    Add buttonStyles.js with button constants

---
+++ src/constants/buttonStyles.js
@@ -0,0 +1,12 @@
+module.exports = {
+    PRIMARY: 1,
+    SECONDARY: 2,
+    SUCCESS: 3,
+    DANGER: 4,
+    LINK: 5,
+    1: 'PRIMARY',
+    2: 'SECONDARY',
+    3: 'SUCCESS',
+    4: 'DANGER',
+    5: 'LINK'
+}
