08fce2fe31a1619b17f9985b2bf8981d9506e555
ELECTR0ZED <ELECTR0ZED@users.noreply.github.com>
2024-03-19 14:07:02 +0000

    Add parameters to validateInteraction function

--- src/utils/validateInteraction.js
+++ src/utils/validateInteraction.js
@@ -2,6 +2,9 @@ const nacl = require('tweetnacl');
 /**
  * Validate the interaction received from Discord.
  * @param {string} publicKey - The public key to verify the signature with.
+ * @param {string} signature - The signature to verify.
+ * @param {string} timestamp - The timestamp of the interaction.
+ * @param {string} body - The body of the interaction.
  * @returns {boolean} Whether the interaction is valid.
  */
 function isValid(publicKey, signature, timestamp, body) {
