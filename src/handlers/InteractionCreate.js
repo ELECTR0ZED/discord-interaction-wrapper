f3416f08ab513c3b7492873475a40a7e582eb7e5
ELECTR0ZED <ELECTR0ZED@users.noreply.github.com>
2024-03-21 21:44:26 +0000

    Add support for AutoComplete interaction type

--- src/handlers/InteractionCreate.js
+++ src/handlers/InteractionCreate.js
@@ -1,12 +1,16 @@
 const CommandInteraction = require('../structures/CommandInteraction');
 const MessageComponentInteraction = require('../structures/MessageComponentInteraction');
+const AutoComplete = require('../structures/AutoComplete');
+const InteractionTypes = require('../constants/InteractionTypes');
 
 
 module.exports = (interaction) => {
     switch (interaction.type) {
-        case 2:
+        case InteractionTypes.APPLICATION_COMMAND:
             return new CommandInteraction(interaction);
-        case 3:
+        case InteractionTypes.MESSAGE_COMPONENT:
             return new MessageComponentInteraction(interaction);
+        case InteractionTypes.APPLICATION_COMMAND_AUTOCOMPLETE:
+            return new AutoComplete(interaction);
     }
 }
